1f58fee22ff2ec99ed0367e936b386f3e3467125
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-15 11:33:17 +0100

    Add routes

--- api/api.js
+++ api/api.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const userRouter = require("./routers/user");
+
 const api = express();
 
 api.use(cors());
 api.use(express.json());
+
+api.use("/user", userRouter);
 
 module.exports = api;
