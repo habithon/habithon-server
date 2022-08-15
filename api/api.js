a0f05c3a0506b8f03fd37baa74f41c010f5e7589
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-15 17:55:02 +0100

    Add goals route

--- api/api.js
+++ api/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const userRouter = require("./routers/user");
+const goalsRouter = require("./routers/goals");
 
 const api = express();
 
@@ -9,5 +10,6 @@ api.use(cors());
 api.use(express.json());
 
 api.use("/user", userRouter);
+api.use("/goals", goalsRouter);
 
 module.exports = api;
