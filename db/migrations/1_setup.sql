e9623f0855ad91ef41661bdc9ea23beac43fce35
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-15 11:34:38 +0100

    Add db

--- db/migrations/1_setup.sql
+++ db/migrations/1_setup.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS userAccount;
+DROP TABLE IF EXISTS user_account;
 
-CREATE TABLE userAccount (
+CREATE TABLE user_account (
     id SERIAL PRIMARY KEY,
     username VARCHAR(30) NOT NULL,
     password CHAR(60) NOT NULL
